4445ae07cf32fa26f3953af3fd2090c6acb6f520
David Rautureau <david.rautureau@sonarsource.com>
2016-05-10 11:13:42 +0200

    Setup QA

--- travis.sh
+++ travis.sh
@@ -4,29 +4,25 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v21 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v28 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
+
+installTravisTools
 
 case "$TEST" in
 
 ci)
-  mvn verify -B -e -V
+  SONAR_PROJECT_VERSION=`maven_expression "project.version"`
+
+  # Do not deploy a SNAPSHOT version but the release version related to this build
+  set_maven_build_version $TRAVIS_BUILD_NUMBER
+
+  # the profile "deploy-sonarsource" is defined in parent pom v28+
+  mvn deploy \
+      -Pdeploy-sonarsource \
+      -B -e -V
   ;;
-
-plugin|ruling)
-  installTravisTools
-
-  mvn package -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
-
-  if [ "$SQ_VERSION" = "DEV" ] ; then
-    build_snapshot "SonarSource/sonarqube"
-  fi
-
-  cd its/$TEST
-  mvn -DjavaVersion="LATEST_RELEASE" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false test
-  ;;
-
 *)
   echo "Unexpected TEST mode: $TEST"
   exit 1
