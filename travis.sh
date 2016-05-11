19ad86e79cf7e02322716350dce27be63909815a
David Rautureau <david.rautureau@sonarsource.com>
2016-05-11 15:56:58 +0200

    Enable regular way to build a SonarSource Maven project on Travis

--- travis.sh
+++ travis.sh
@@ -13,15 +13,7 @@ installTravisTools
 case "$TEST" in
 
 ci)
-  SONAR_PROJECT_VERSION=`maven_expression "project.version"`
-
-  # Do not deploy a SNAPSHOT version but the release version related to this build
-  set_maven_build_version $TRAVIS_BUILD_NUMBER
-
-  # the profile "deploy-sonarsource" is defined in parent pom v28+
-  mvn deploy \
-      -Pdeploy-sonarsource \
-      -B -e -V
+  regular_mvn_build_deploy_analyze
   ;;
 *)
   echo "Unexpected TEST mode: $TEST"
