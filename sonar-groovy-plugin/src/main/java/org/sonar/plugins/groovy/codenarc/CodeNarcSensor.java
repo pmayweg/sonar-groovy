fb0f7ff7d096ebba1a0bd5a192838dffa242494c
Vladimir Orany <vladimir@orany.cz>
2017-10-26 10:01:28 +0200

    fixed import, used proper method from settings

--- sonar-groovy-plugin/src/main/java/org/sonar/plugins/groovy/codenarc/CodeNarcSensor.java
+++ sonar-groovy-plugin/src/main/java/org/sonar/plugins/groovy/codenarc/CodeNarcSensor.java
@@ -22,6 +22,7 @@ package org.sonar.plugins.groovy.codenarc;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -73,9 +74,9 @@ public class CodeNarcSensor implements Sensor {
     // Should we reuse existing report from CodeNarc ?
     if (context.settings().hasKey(GroovyPlugin.CODENARC_REPORT_PATH)) {
       // Yes
-      String codeNarcReportPath = context.settings().getString(GroovyPlugin.CODENARC_REPORT_PATH);
+      List<String> codeNarcReportPaths = context.settings().getStringArray(GroovyPlugin.CODENARC_REPORT_PATH);
       List<File> reports = new ArrayList<File>();
-      for (String path : codeNarcReportPath.split("\\s*,\\s*")) {
+      for (String path : codeNarcReportPaths) {
         File report = context.fileSystem().resolvePath(path);
         if (!report.isFile()) {
           LOG.warn("Groovy report " + GroovyPlugin.CODENARC_REPORT_PATH + " not found at {}", report);
